abac6e9e96806f17f1d142c7e6f2ad0589220f13
Vidar Eldøy <vidar@eldoy.com>
2026-02-26 21:05:40 +0100

    fix: use full lodash

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-var set = require('lodash.set')
-var get = require('lodash.get')
-var pick = require('lodash.pick')
-var omit = require('lodash.omit')
-var clone = require('lodash.clonedeep')
+var set = require('lodash/set')
+var get = require('lodash/get')
+var pick = require('lodash/pick')
+var omit = require('lodash/omit')
+var clone = require('lodash/cloneDeep')
 
 module.exports = {
   get,
